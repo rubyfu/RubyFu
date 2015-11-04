0f84f3bbe9fb29bbc9d61b766f0d4730d2f38552
KINGSABRI <king.sabri@gmail.com>
2015-11-05 01:08:36 +0300

    Update styles/header.js

---
+++ styles/header.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+window.localStorage.removeItem(":keyword");
+
+$(document).ready(function() {
+
+function appendHeader() {
+
+  var div = document.createElement('div');
+  div.innerHTML = '<header id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="http://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png"></a></div></div></div></header>';
+
+    $('.book').before(div.innerHTML);
+
+  };
+
+  function rerenderNavbar() {
+    $('#header').remove();
+    appendHeader();
+  };
+
+  rerenderNavbar();
+
+});
+
+};
