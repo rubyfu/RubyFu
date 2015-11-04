7ae2aff1989644213e7bba8ee3e4bda3d44ecbd1
KINGSABRI <king.sabri@gmail.com>
2015-11-05 00:24:18 +0300

    Update styles/header.js

--- styles/header.js
+++ styles/header.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 function appendHeader() {
 
   var div = document.createElement('div');
-  div.innerHTML = '<header id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="http://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png"></a></div><div class="arangodb_version">Where ruby goes evil!</div></div></div></header>';
+  div.innerHTML = '<header id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="http://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png"></a></div></div></div></header>';
 
     $('.book').before(div.innerHTML);
 
