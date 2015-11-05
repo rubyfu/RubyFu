683b59f9f8c460c5ebba481d7eadc3d627ef7e63
KINGSABRI <king.sabri@gmail.com>
2015-11-06 02:52:44 +0300

    Update styles/header.js

---
+++ styles/header.js
@@ -0,0 +1,1 @@
+$( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" style="height:48px"></a></div></div></div></div>');
