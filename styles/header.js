f85a24aff2c8838a56c17e2b291039722d675b25
KINGSABRI <king.sabri@gmail.com>
2015-11-06 03:04:16 +0300

    Update styles/header.js

--- styles/header.js
+++ styles/header.js
@@ -1,3 +1,3 @@
-$( document ).ready(function() {
+window.onload = function(){
  $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" style="height:48px"></a></div></div></div></div>');
-});
+};
