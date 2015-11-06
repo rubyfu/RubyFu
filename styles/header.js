76858ed21f8ab77b792944dcd2d2bc010468aba1
KINGSABRI <king.sabri@gmail.com>
2015-11-06 03:08:59 +0300

    Update styles/header.js

--- styles/header.js
+++ styles/header.js
@@ -1,3 +1,3 @@
-window.onload = function(){
- $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" style="height:48px"></a></div></div></div></div>');
+ window.onload = function(){
+ $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" style="height: 50px;width: 67px;"></a></div></div></div></div>');
 };
