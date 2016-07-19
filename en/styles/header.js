36b0f235a457e61710a232f0d37ee129a3604d1b
KINGSABRI <king.sabri@gmail.com>
2016-07-19 16:42:50 +0300

    Add styles folder as symlink to fix style issue

--- en/styles/header.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
- $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" class="logo_rubyfu"></a></div></div></div></div>');
-
-$('.fa-spin').nextAll().remove()
-};
