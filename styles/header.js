15239d84444fab4ae18487076e77b80b1c1ea5c2
KINGSABRI <king.sabri@gmail.com>
2016-12-08 10:55:57 +0000

    Track 1 files into repository.
    - modified styles/header.js
    
    Auto commit by GitBook Editor

--- styles/header.js
+++ styles/header.js
@@ -3,3 +3,13 @@ window.onload = function(){
 
 $('.fa-spin').nextAll().remove()
 };
+
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-88660585-1', 'auto');
+ga('send', 'pageview');
