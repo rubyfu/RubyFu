ecc6b89783da1442256be02ce88101299e325f57
KINGSABRI <king.sabri@gmail.com>
2016-12-23 16:19:15 +0000

    Track 1 files into repository.
    - modified styles/header.js
    
    Auto commit by GitBook Editor

--- styles/header.js
+++ styles/header.js
@@ -1,6 +1,9 @@
 window.onload = function(){
  $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" class="logo_rubyfu"></a></div></div></div></div>');
 
+    $(".book-body .body-inner").niceScroll({cursorcolor:"#b21818"});
+    $(".book-summary").niceScroll({cursorcolor:"#b21818"});
+  
 $('.fa-spin').nextAll().remove()
 };
 
