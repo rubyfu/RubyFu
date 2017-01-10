197e77a490b9390da60cfd3099e30c6de157b9eb
KINGSABRI <king.sabri@gmail.com>
2017-01-10 19:24:45 +0300

    Updates styles/header.js
    Auto commit by GitBook Editor

--- styles/header.js
+++ styles/header.js
@@ -1,5 +1,5 @@
 window.onload = function(){
- $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" class="logo_rubyfu"></a></div></div></div></div>');
+ $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="https://rubyfu.net/content/images/other/logo.png" class="logo_rubyfu"></a></div></div></div></div>');
 
     $(".book-body .body-inner").niceScroll({cursorcolor:"#b21818"});
     $(".book-summary").niceScroll({cursorcolor:"#b21818"});
